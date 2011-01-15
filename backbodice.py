e6f3fd5e8546162ef0f5ae97717b35f6d2bf4836
Steve Conklin <steve@conklinhouse.com>
2011-01-15 10:46:59 -0600

    backbodice 08-15-2010 17:37

--- backbodice.py
+++ backbodice.py
@@ -108,8 +108,7 @@ class DrawBackBodice(inkex.Effect):
            uhc=self.options.upper_hip_circumference*convert_to_pixels
            lhc=self.options.lower_hip_circumference*convert_to_pixels
            ssl=self.options.side_seam_length*convert_to_pixels
-           begin_pattern_x=3*convert_to_pixels               #Pattern begins in upper left corner x=3"
-           begin_pattern_y=3*convert_to_pixels               # same...y=3" 
+          
            referenceline_color='gray'
            referenceline_width='7'
            referenceline_fill='gray'
@@ -127,6 +126,9 @@ class DrawBackBodice(inkex.Effect):
            dartdot_color = 'black'
            dartdot_width = .10
            dartdot_fill='black'
+
+           begin_pattern_x=1*convert_to_pixels               #Pattern begins in upper left corner x=1"
+           begin_pattern_y=1*convert_to_pixels               # same...y=1" 
            
 
            # Create a special layer to draw the pattern.
@@ -145,16 +147,36 @@ class DrawBackBodice(inkex.Effect):
            self.DrawMyLine(my_layer,Ax,Ay,Bx,By,referenceline_color,referenceline_width,'AB')
            self.DrawMyDot(my_layer,Ax,Ay,dot_radius,dot_color,dot_width,dot_fill,'A')
            self.DrawMyDot(my_layer,Bx,By,dot_radius,dot_color,dot_width,dot_fill,'B')
+           New_Bx=(Bx+wc/4)
+           New_By=By
+           self.DrawMyLine(my_layer,Bx,By,New_Bx,New_By,'Green',referenceline_width,'BNew_B')
            #_______________
-           # Create bust-line point C along AB, measuring from A, at length = back-bodice-length/2. 
+           # Create bust-line point C along AB, measuring from A, at length = back-bodice-length/2 
            Cx=Ax
            Cy=Ay+(bbl/2)
+           #Create bust reference line length = (bc/4) - 1cm (to move side seam back 1cm for looks)
+           Dx=Cx+(bc/4)-(1*convert_to_inches*convert_to_pixels)
+           Dy = Cy
            self.DrawMyDot(my_layer,Cx,Cy,dot_radius,dot_color,dot_width,dot_fill,'C') 
+           self.DrawMyLine(my_layer,Cx,Cy,Dx,Dy,referenceline_color,referenceline_width,'CD')
            #_______________
-           # Create armpit-line point D along AB, measuring from A, at length = back-bodice-length/3. 
-           Dx=Ax
-           Dy=Ay+(bbl/3)
-           self.DrawMyDot(my_layer,Dx,Dy,dot_radius,dot_color,dot_width,dot_fill,'D') 
+           # Create armpit-line point E along AB, length = back-bodice-length/3. 
+           Ex=Ax
+           Ey=Ay+(bbl/3)
+           self.DrawMyDot(my_layer,Ex,Ey,dot_radius,dot_color,dot_width,dot_fill,'E') 
+           # Create armpit reference line EF, length = bad/2 - 1cm
+           Fx=Ex + (bad/2) - (1*convert_to_inches*convert_to_pixels)
+           Fy=Ey
+           self.DrawMyLine(my_layer,Ex,Ey,Fx,Fy,referenceline_color,referenceline_width,'EF')
+           self.DrawMyDot(my_layer,Fx,Fy,dot_radius,dot_color,dot_width,dot_fill,'F') 
+           #_______________  
+           # Find Dart Intake Size - (bust circumference - waist circumference), divide by 8.
+           my_dart_depth=((bc-wc)/8)
+           # Create BO Waist reference line = wc/4 + (2*my_dart_depth) - 1cm (move side seam back by 1cm for looks)
+           Ox=New_Bx
+           Oy=By
+           self.DrawMyLine(my_layer,Bx,By,Ox,Oy,referenceline_color,referenceline_width,'BO')
+           self.DrawMyDot(my_layer,Ox,Oy,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'O')      
            #_______________                
            #At A, draw line length = shoulderwidth/2 + 2cm, perpendicular to AB. Mark endpoint as G. (2cm (2/3in) will be the width of the shoulder dart)
            Gx=Ax+(sw/2)+(2*convert_to_inches*convert_to_pixels)
@@ -162,138 +184,100 @@ class DrawBackBodice(inkex.Effect):
            self.DrawMyLine(my_layer,Ax,Ay,Gx,Gy,referenceline_color,referenceline_width,'AG')
            self.DrawMyDot(my_layer,Gx,Gy,dot_radius,dot_color,dot_width,dot_fill,'G')
            #_______________
-           #On AG, measuring from A, mark point E at length = (neck-circumference/6 + .5cm (.2in). E marks point for Neck opening.         
-           Ex=Ax+(nc/6)+(.5*convert_to_inches*convert_to_pixels)
-           Ey=Ay
-           self.DrawMyDot(my_layer,Ex,Ey,dot_radius,dot_color,dot_width,dot_fill,'E')
-           #_______________
-           #On AB, measuring from A, mark point F at length = 2.5cm, or 1 inch. F marks point for Neck depth.
-           Fx=Ax
-           Fy=Ay+(1*convert_to_pixels)
-           self.DrawMyDot(my_layer,Fx,Fy,dot_radius,dot_color,dot_width,dot_fill,'F')          
-           #_______________
-           controlx=Ex
-           controly=Fy
-           self.DrawMyQCurve(my_layer,Fx,Fy,Ex,Ey,controlx,controly,patternline_color,patternline_width,'FE')
-           #_______________
-           #At G, draw line: length = 4cm, perpendicular to AG. Mark endpoint as H. (4cm) is average depth of shoulder slope)
-           #!!!! Change later to use an individual's actual shoulder slope
-           Hx=Gx
-           Hy=Gy+(4*convert_to_inches)*(convert_to_pixels)
-           self.DrawMyLine(my_layer,Gx,Gy,Hx,Hy,referenceline_color,referenceline_width,'GH')
-           #_______________
-           #Draw line from E to H. Creates sloped shoulder line EH.
-           self.DrawMyLine(my_layer,Ex,Ey,Hx,Hy,patternline_color,patternline_width,'EH')
+           #On AG, measuring from A, mark point H at length = (neck-circumference/6 + .5cm (.2in). H marks point for Neck opening.         
+           Hx=Ax+(nc/6)+(.5*convert_to_inches*convert_to_pixels)
+           Hy=Ay
            self.DrawMyDot(my_layer,Hx,Hy,dot_radius,dot_color,dot_width,dot_fill,'H')
            #_______________
-           #On EH, find midpoint I. Creates midpoint of back shoulder dart.
-           # Find dart points K and L where K and L are 1cm away from dart midpoint I 
-           Ix=Ex+((Hx-Ex)/2)
-           Iy=Ey+((Hy-Ey)/2)
-           my_slope = (Hy-Ey)/(Hx-Ex)
+           #On AB, measuring from A, mark point I at length = 2.5cm, or 1 inch. I marks point for Neck depth.
+           Ix=Ax
+           Iy=Ay+(1*convert_to_pixels)
+           self.DrawMyDot(my_layer,Ix,Iy,dot_radius,dot_color,dot_width,dot_fill,'I')          
+           # Draw curve from I to H to form neck opening
+           controlx=Hx
+           controly=Iy
+           self.DrawMyQCurve(my_layer,Ix,Iy,Hx,Hy,controlx,controly,patternline_color,patternline_width,'IH')
+           #_______________
+           #Find point J perpendicular from AG, length = 4cm. (4cm is average depth of shoulder slope)
+           #!!!! Change later to use an individual's actual shoulder slope
+           Jx=Gx
+           Jy=Gy+(4*convert_to_inches)*(convert_to_pixels)
+           self.DrawMyLine(my_layer,Gx,Gy,Jx,Jy,referenceline_color,referenceline_width,'GJ')
+           #_______________
+           #Draw line from H to J. Creates sloped shoulder line HJ.
+           self.DrawMyLine(my_layer,Hx,Hy,Jx,Jy,patternline_color,patternline_width,'HJ')
+           self.DrawMyDot(my_layer,Jx,Jy,dot_radius,dot_color,dot_width,dot_fill,'J')
+          # Create top of armscye FJ
+           self.DrawMyLine(my_layer,Fx,Fy,Jx,Jy,patternline_color,patternline_width,'FJ')
+           #_______________
+           #On HJ find midpoint K. Creates midpoint of back shoulder dart.
+           # Find dart points L and M each are 1cm away from dart midpoint K
+           Kx=Hx+((Jx-Hx)/2)
+           Ky=Hy+((Jy-Hy)/2)
+           self.DrawMyDot(my_layer,Kx,Ky,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'K')
+           my_slope = (Jy-Hy)/(Jx-Hx)
            my_radius = 1*convert_to_inches*convert_to_pixels
-           Kx,Ky = self.GetCoordsFromSlope(my_layer,Ix,Iy,my_slope,my_radius)
-           Lx,Ly = self.GetCoordsFromSlope(my_layer,Ix,Iy,my_slope,-my_radius)
-           # Find dart end point J, 9cm perpendicular from EH at point I (9cm is average depth of back shoulder dart)
+           Lx,Ly = self.GetCoordsFromSlope(my_layer,Kx,Ky,my_slope,my_radius)
+           Mx,My = self.GetCoordsFromSlope(my_layer,Kx,Ky,my_slope,-my_radius)
+           self.DrawMyDot(my_layer,Lx,Ly,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'L')
+           self.DrawMyDot(my_layer,Mx,My,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'M')
+           # Find dart end point N, 9cm perpendicular from HJ at point K (9cm is average depth of back shoulder dart)
            my_perpendicular_slope = -(1/my_slope)
            my_radius = 9*convert_to_inches*convert_to_pixels
-           Jx,Jy = self.GetCoordsFromSlope(my_layer,Ix,Iy,my_perpendicular_slope,my_radius) 
-           # Draw lines KJ, LJ, and dots J,K,L
-           self.DrawMyLine(my_layer,Kx,Ky,Jx,Jy,dartline_color,dartline_width,'KJ')
-           self.DrawMyLine(my_layer,Lx,Ly,Jx,Jy,dartline_color,dartline_width,'LJ')
-           self.DrawMyDot(my_layer,Jx,Jy,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'J')
-           self.DrawMyDot(my_layer,Kx,Ky,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'K')
-           self.DrawMyDot(my_layer,Lx,Ly,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'L')
+           Nx,Ny = self.GetCoordsFromSlope(my_layer,Kx,Ky,my_perpendicular_slope,my_radius) 
+           self.DrawMyDot(my_layer,Nx,Ny,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'N')
+           # Draw lines KN,LN,MN
+           self.DrawMyLine(my_layer,Kx,Ky,Nx,Ny,dartline_color,dartline_width,'KN')
+           self.DrawMyLine(my_layer,Lx,Ly,Nx,Ny,dartline_color,dartline_width,'LN')
+           self.DrawMyLine(my_layer,Mx,My,Nx,Ny,dartline_color,dartline_width,'MN')
            #_______________
-           # Create Back Darts --> Center Back Dart and Back Dart
-           # Find Dart Intake Size - this measurement is in relationship to the dart size for the skirt
-           # So find the dart size that will be needed for the skirt using the larger measurement of upper or lower hip circumference
-           # Take widest hip circumference, subtract waist size, divide by 9.   That's the Dart depth except for back center dart which is half that depth
-           if (uhc > lhc):
-                hip_minus_waist = (uhc-wc)
-           else:
-                hip_minus_waist = (lhc-wc)
-           my_dart_depth=(hip_minus_waist/9)
-           my_back_center_dart_depth=my_dart_depth/2
-           # Make Center Back Dart point M horizontally from point B. Draw Center Back Dart line MF
-           Mx=Bx+my_back_center_dart_depth
-           My=By
-           self.DrawMyLine(my_layer,Mx,My,Fx,Fy,patternline_color,patternline_width,'FM')           
-           self.DrawMyDot(my_layer,Mx,My,dot_radius,dot_color,dot_width,dot_fill,'M')
-           #Make Waist Dart midpoint N horizontally from point B at bust distance. Dart ends at point O horizontally from C at bustpointdistance/2.
-           #Find Dart Legs at points P & Q at my_dart_depth away from point N
-           Nx=Bx+bpd/2
-           Ny=By
-           Ox=Cx+bpd/2
-           Oy=Cy
-           # Draw lines PO, QO
-           my_slope = 0    #horizontal line
-           my_radius = my_dart_depth
-           Px,Py = self.GetCoordsFromSlope(my_layer,Nx,Ny,my_slope,my_radius)
-           Qx,Qy = self.GetCoordsFromSlope(my_layer,Nx,Ny,my_slope,-my_radius)
-           self.DrawMyLine(my_layer,Px,Py,Ox,Oy,dartline_color,dartline_width,'PO')
-           self.DrawMyLine(my_layer,Qx,Qy,Ox,Oy,dartline_color,dartline_width,'QO')
-           self.DrawMyDot(my_layer,Ox,Oy,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'O')
+           # Create Back Waist Dart
+           #Find Dart midpoint P on BO at bustpointdistance/2. 
+           Px=Bx+(bpd/2)
+           Py=By
            self.DrawMyDot(my_layer,Px,Py,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'P')
+           # Find bust dart apex point Q on line C at bpd/2
+           Qx=Cx+(bpd/2)
+           Qy=Cy
            self.DrawMyDot(my_layer,Qx,Qy,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'Q')
+           # Find dart points R & S at my_dart_depth away from point N
+           Rx = Px-my_dart_depth
+           Ry = Py
+           self.DrawMyDot(my_layer,Rx,Ry,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'R')
+           Sx = Px+my_dart_depth
+           Sy = Py
+           self.DrawMyDot(my_layer,Sx,Sy,dartdot_radius,dartdot_color,dartdot_width,dartdot_fill,'S')
+           # Draw lines PQ,RQ,SQ
+           self.DrawMyLine(my_layer,Px,Py,Qx,Qy,dartline_color,dartline_width,'PQ')
+           self.DrawMyLine(my_layer,Rx,Ry,Qx,Qy,dartline_color,dartline_width,'RQ')
+           self.DrawMyLine(my_layer,Sx,Sy,Qx,Qy,dartline_color,dartline_width,'SQ')
+           #________________
+           # Draw waist pattern line from B to S
+           self.DrawMyLine(my_layer,Bx,By,Sx,Sy,patternline_color,patternline_width,'BP')
            #_______________
-           # Draw waist reference lines
-           # Waist/4 -1cm + 2.5 Dart depths = waist reference line. End point is R. 
-           # Mark point S 1cm vertical to point R
-           Rx = Bx+ (wc/4)-(1*convert_to_inches*convert_to_pixels) + (2.5*my_dart_depth)
-           Ry = By
-           Sx = Rx
-           Sy = Ry - (1*convert_to_inches*convert_to_pixels)
-           self.DrawMyLine(my_layer,Bx,By,Rx,Ry,referenceline_color,referenceline_width,'BR')
-           self.DrawMyDot(my_layer,Sx,Sy,dot_radius,dot_color,dot_width,dot_fill,'S')         
+           # Mark point T 1cm vertical to point O
+           Tx = Ox
+           Ty = Oy - (90)           #1*convert_to_inches*convert_to_pixels)
+           self.DrawMyDot(my_layer,Tx,Ty,dot_radius,dot_color,dot_width,dot_fill,'T')
            #_______________
-           # Draw waist pattern line from M to Q
-           self.DrawMyLine(my_layer,Mx,My,Qx,Qy,patternline_color,patternline_width,'MQ')
-           # Draw waist curve from Q to S, control point x is Qx+(Rx-Qx)*(3/4)
-           #Qxcontrollength=.25
-           #Qycontrolheight=0
-           C1x=Qx+((Sx-Qx)*(.25))
-           C1y=Qy+0
-           #Sxcontrollength=.75
-           #Sycontrolheight=.50
-           C2x=Qx+((Sx-Qx)*(.75))
-           C2y=Sy-((Sy-Ry)*(.5))
-           my_pathdefinition='M '+str(Qx)+','+str(Qy)+'  C '+str(C1x)+','+str(C1y)+'  '+str(C2x)+','+str(C2y)+' '+str(Sx)+','+str(Sy)
-           self.DrawMyCurve(my_layer,my_pathdefinition,patternline_color,patternline_width,'QS')
+           # Draw partial side seam TD
+           self.DrawMyLine(my_layer,Tx,Ty,Dx,Dy,patternline_color,patternline_width,'TD')
+
            #_______________
-           # From D, find point T perpendicular to AB on FM.
-           # Extend T horizontally, length = back-armpit-distance/2. Mark endpoint as U. Creates armpit line TU.
-           # From H draw line to U to form top segment of armscye.
-           m = (My-Fy)/(Mx-Fx)   #slope of FM
-           Ty=Dy    #T  is horizontal to D
-           # m = (My-Ty)/(Mx-Tx)
-           # m(Mx-Tx)=(My-Ty)
-           # (Mx-Tx)=(My-Ty)/m
-           Tx= Mx - ((My-Ty)/m)
-           Ux=Tx+(bad/2)
-           Uy=Ty # horizontal line
-           self.DrawMyLine(my_layer,Tx,Ty,Ux,Uy,referenceline_color,referenceline_width,'TU')  
-           self.DrawMyLine(my_layer,Hx,Hy,Ux,Uy,referenceline_color,referenceline_width,'HU') 
-           self.DrawMyDot(my_layer,Tx,Ty,dot_radius,dot_color,dot_width,dot_fill,'T')         
-           self.DrawMyDot(my_layer,Ux,Uy,dot_radius,dot_color,dot_width,dot_fill,'U') 
+           # Draw waist curve from S to T, control point x is midpoint between S & T, control y is Sy+1px
+           controlx=Rx+((Tx-Sx)*(.5))
+           controly=Ry+1
+           #my_pathdefinition='M '+str(Rx)+','+str(Ry)+'  C '+str(C1x)+','+str(C1y)+' '+str(Tx)+','+str(Ty)
+           #self.DrawMyCurve(my_layer,my_pathdefinition,patternline_color,patternline_width,'RT')
+           self.DrawMyQCurve(my_layer,Tx,Ty,Rx,Ry,controlx,controly,'pink',patternline_width,'RT')
+ 
            #_______________
-           # From C, find point V perpendicular to AB on FM. 
-           # Extend V horizontally to point W, length = (bust-circumference/4)-1cm.The 1cm is removed from back, 1cm will be added to front. 
-           m = (My-Fy)/(Mx-Fx)   #slope of FM
-           Vy=Cy  # horizontal line - y stays the same
-           Vx=Mx-((My-Vy)/m)
-           Wx=Vx+(bc/4) - 1*convert_to_inches*convert_to_pixels
-           Wy=Vy # horizontal line - y stays the same
-           self.DrawMyLine(my_layer,Vx,Vy,Wx,Wy,referenceline_color,referenceline_width,'VW')  
-           self.DrawMyDot(my_layer,Vx,Vy,dot_radius,dot_color,dot_width,dot_fill,'V')         
-           self.DrawMyDot(my_layer,Wx,Wy,dot_radius,dot_color,dot_width,dot_fill,'W') 
-           #_______________
-           # At S, draw line through W to point X, length = side-seam-length - 2cm. Creates side seam, leaves room for arm to move
-           my_slope = (Sy-Wy)/(Sx-Wx)
-           my_length=ssl - 2*convert_to_inches*convert_to_pixels
-           Xx,Xy=self.GetCoordsFromSlope(my_layer,Sx,Sy,my_slope,my_length)
-           self.DrawMyLine(my_layer,Sx,Sy,Xx,Xy,patternline_color,patternline_width,'SX')  
-           self.DrawMyDot(my_layer,Xx,Xy,dot_radius,dot_color,dot_width,dot_fill,'X')   
+           # At T, draw line through W to point X, length = side-seam-length - 2cm. Creates side seam, lowering side seam by 2cm leaves room for arm to move
+           ##my_slope = (Sy-Wy)/(Sx-Wx)
+           ##my_length= ssl - (2*convert_to_inches*convert_to_pixels)
+           ##Xx,Xy=self.GetCoordsFromSlope(my_layer,Sx,Sy,my_slope,my_length)
+           ##self.DrawMyLine(my_layer,Sx,Sy,Xx,Xy,patternline_color,patternline_width,'SX')  
+           ##self.DrawMyDot(my_layer,Xx,Xy,dot_radius,dot_color,dot_width,dot_fill,'X')   
            #_______________
            # From U, draw smooth curve to X. Creates armscye and completes Back Bodice Block Pattern. 
            #Uxcontrollength=0
@@ -303,9 +287,9 @@ class DrawBackBodice(inkex.Effect):
            #Xxcontrollength=.75
            #Xycontrolheight=.50
            #C2x=Ux+((Xx-Ux)*(.75))
-           #C2y=Ux+((Xy-Uy)*(.5))
-           my_pathdefinition='M '+str(Ux)+','+str(Uy)+'  Q '+str(Ux)+','+str(Xy)+' '+str(Xx)+','+str(Xy)
-           self.DrawMyCurve(my_layer,my_pathdefinition,patternline_color,patternline_width,'UX')     
+           #C2y=Uy+((Xy-Uy)*(.5))
+           ##my_pathdefinition='M '+str(Ux)+','+str(Uy)+'  Q '+str(Ux)+','+str(Xy)+' '+str(Xx)+','+str(Xy)
+           ##self.DrawMyCurve(my_layer,my_pathdefinition,patternline_color,patternline_width,'UX')     
            #_______________ 
            #                            
            # 
