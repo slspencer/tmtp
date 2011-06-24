06dcc887a69ed0bb4ff131f58a8a8a9069687231
Steve Conklin <steve@conklinhouse.com>
2011-06-23 22:49:38 -0500

    Fix tooltip display
    Closes #16.
    
    Tool tips were not displaying correctly because the coordinates
    returned by the event object are the ones originally set, and
    not the screen coordinates after transformation. It is
    necessary to retrieve the transformation matrix and apply
    it to decide where to display the tooltip.
    
    Signed-off-by: Steve Conklin <sconklin@canonical.com>

--- standalone/tmtp_mouse.js
+++ standalone/tmtp_mouse.js
@@ -8,10 +8,27 @@ function init(evt) {
 
 function ShowTooltip(evt) {
     // Put tooltip in the right position, change the text and make it visible
-    tooltip.setAttributeNS(null,"x",evt.clientX+10);
-    tooltip.setAttributeNS(null,"y",evt.clientY+30);
-    tooltip.firstChild.data = evt.target.getAttributeNS(null,"id");
-    //tooltip.firstChild.data = evt.target.getAttributeNS(null,"mouseovertext");
+    //tooltip.setAttributeNS(null,"x",evt.clientX+10);
+    //tooltip.setAttributeNS(null,"y",evt.clientY+30);
+ 
+    //alert(evt.currentTarget.cx.baseVal.value);
+
+    // works on non-transformed objects
+    //tooltip.setAttributeNS(null,"x",evt.currentTarget.cx.baseVal.value);
+    //tooltip.setAttributeNS(null,"y",evt.currentTarget.cy.baseVal.value);
+
+    var svg = document.getElementsByTagName('svg')[0];
+    var pt  = svg.createSVGPoint();
+    var matrix  = evt.currentTarget.getCTM();
+    pt.x = evt.currentTarget.cx.baseVal.value;
+    pt.y = evt.currentTarget.cy.baseVal.value;
+    rpt = pt.matrixTransform(matrix);
+    tooltip.setAttributeNS(null,"x",rpt.x);
+    tooltip.setAttributeNS(null,"y",rpt.y - 20);
+
+    var ttl = evt.target.getAttributeNS(null,"id").split(".");
+    ttl.shift();
+    tooltip.firstChild.data = ttl.join(".");
     tooltip.setAttributeNS(null,"visibility","visible");
 }
 
