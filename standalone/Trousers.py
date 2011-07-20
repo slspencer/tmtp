1f4ddf9cba35ec91c6c379f7f9acc05bc2696bd7
susan <susan.spencer@gmail.com>
2011-07-20 04:11:15 -0500

    Changed formulas for points p4 & p5 to balance pants leg equally on both sides of crease line between p16 & p14

--- standalone/Trousers.py
+++ standalone/Trousers.py
@@ -223,8 +223,9 @@ class PatternDesign():
         tf.add(Point('reference', 'p11',  tf.D.x + (cd.seat*0.25) - (0.5*cm_to_px*seatRatio) , tf.D.y, 'point_style'))
         tf.add(Point('reference', 'p16',  tf.p2.x + (abs(tf.p11.x - tf.p2.x)*0.5), tf.p2.y, 'point_style'))
 
-        tf.add(Point('reference', 'p4', tf.E.x - (3.75*cm_to_px*insideLegRatio), tf.E.y, 'point_style'))
-        tf.add(Point('reference', 'p5', tf.F.x - (2.8*cm_to_px*insideLegRatio), tf.F.y, 'point_style'))
+        #tf.add(Point('reference', 'p4', tf.E.x - (3.75*cm_to_px*insideLegRatio), tf.E.y, 'point_style'))
+        tf.add(Point('reference', 'p4', (tf.p16.x - tf.D.x) - (3.75*cm_to_px*insideLegRatio), tf.E.y, 'point_style'))
+        tf.add(Point('reference', 'p5', (tf.p16.x - tf.D.x) - (2.8*cm_to_px*insideLegRatio), tf.F.y, 'point_style'))
 
         m = (tf.p5.y - tf.p4.y)/(tf.p5.x-tf.p4.x)
         b = tf.p4.y - (m*tf.p4.x)
