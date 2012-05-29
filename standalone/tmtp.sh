27beb23de7bb8cef96e30c586dfe49a65e01f5c4
Susan Spencer <susan.spencer@gmail.com>
2012-05-29 17:07:12 -0500

    Created tmtp.sh startup script

--- standalone/tmtp.sh
+++ standalone/tmtp.sh
@@ -1,7 +1,7 @@
 cd ./src/tmtp/standalone/
 
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/Misc/New_Jeans2.py --styles=tmtp_styles.json foo.svg
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/Misc/my_jeans.py --styles=tmtp_styles.json foo.svg
+#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=New_Jeans2.py --styles=tmtp_styles.json foo.svg
+#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=my_jeans.py --styles=tmtp_styles.json foo.svg
 
 #./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_SingleWaistDart_MRohr.py --styles=tmtp_styles.json foo.svg
 
@@ -9,7 +9,7 @@ cd ./src/tmtp/standalone/
 
 #./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_3WaistDarts_MRohr.py --styles=tmtp_styles.json foo.svg
 
-./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_3ShoulderTucks_BackNeckDart_MRohr.py --styles=tmtp_styles.json foo.svg
+#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_3ShoulderTucks_BackNeckDart_MRohr.py --styles=tmtp_styles.json foo.svg
 
 #./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_3WaistDarts_Revised_MRohr.py --styles=tmtp_styles.json foo.svg
 
@@ -17,7 +17,8 @@ cd ./src/tmtp/standalone/
 
 
 
-inkscape --file=foo.svg --verb=ZoomPage --select=A.cuttingline --select=B.cuttingline --select=C.cuttingline --verb=SelectionOffset --verb=EditDeselect --verb=FileSave 
+inkscape --file=foo.svg --verb=ZoomPage --select=A.cuttingline --select=B.cuttingline --select=C.cuttingline  --select=D.cuttingline --select=E.cuttingline --select=F.cuttingline --select=G.cuttingline --verb=SelectionOffset --verb=EditDeselect --verb=FileSave 
+
 
 #inkscape --file=foo.svg --export-area-snap -A foo.pdf
 
