85c365fa1c086ac57796ce3e761dd2da3f5a44e5
Susan Spencer <susan.spencer@gmail.com>
2012-06-10 16:39:40 -0500

    File Cleanup

--- standalone/tmtp.sh
+++ standalone/tmtp.sh
@@ -1,32 +1,7 @@
-
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=New_Jeans2.py --styles=tmtp_styles.json foo.svg
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=my_jeans.py --styles=tmtp_styles.json foo.svg
-
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_SingleWaistDart_MRohr.py --styles=tmtp_styles.json foo.svg
-
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_BackNeckDart_MRohr.py --styles=tmtp_styles.json foo.svg
-
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_3WaistDarts_MRohr.py --styles=tmtp_styles.json foo.svg
-
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_3ShoulderTucks_BackNeckDart_MRohr.py --styles=tmtp_styles.json foo.svg
-
-#./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/MRohr/library/library_Bodice_Fitted_3WaistDarts_Revised_MRohr.py --styles=tmtp_styles.json foo.svg
-
-./mkpattern --tooltips --client=./customer/Susancm.json --pattern=./patterns/Aldrich/Mens/library/library_Mens_Classic_Shirt_Aldrich.py --styles=tmtp_styles.json foo.svg
-
-
-
-inkscape --file=foo.svg --verb=ZoomPage --select=A.cuttingline --select=B.cuttingline --select=C.cuttingline  --select=D.cuttingline --select=E.cuttingline --select=F.cuttingline --select=G.cuttingline --verb=SelectionOffset --verb=EditDeselect --verb=FileSave 
-
-
+./mkpattern --tooltips --client=Susancm.json --pattern=SamplePattern.py --styles=tmtp_styles.json foo.svg
+inkscape --file=foo.svg
 #inkscape --file=foo.svg --export-area-snap -A foo.pdf
 
-
-#FileClose
-#EditDelete
-#SelectionOffset
-#ZoomPage
-#--verb=ZoomPage 
  
 
 
