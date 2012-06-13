6ea82fa495a66fd0bd8e8a8c0efdbb38d88714e1
Susan Spencer <susan.spencer@gmail.com>
2012-06-12 22:48:36 -0500

    Removed unused import of json module

--- standalone/tmtpl/pattern.py
+++ standalone/tmtpl/pattern.py
@@ -19,9 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
+#import sys
 import math
-import json
+#import json
 import string
 import re
 import random
