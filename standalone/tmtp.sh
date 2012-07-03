edd158509588e28b0d5f25f75f5e14d21eee90da
Susan Spencer <susan.spencer@gmail.com>
2012-07-03 18:46:06 -0500

    Change default pattern

--- standalone/tmtp.sh
+++ standalone/tmtp.sh
@@ -1,5 +1,7 @@
-./mkpattern --tooltips --verbose --client=Susancm.json --pattern=SamplePattern.py --styles=tmtp_styles.json foo.svg
+./mkpattern --verbose --client=./customer/Susan/Susancm.json --pattern=./patterns/Aldrich/Mens/library/library_Mens_Classic_Shirt_Aldrich.py --styles=tmtp_styles.json foo.svg
+
 inkscape --file=foo.svg
+
 #inkscape --file=foo.svg --export-area-snap -A foo.pdf
 
  
